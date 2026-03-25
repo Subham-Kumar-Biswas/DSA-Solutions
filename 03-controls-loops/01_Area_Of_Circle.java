ae5aed935d36ee6273ad43114334bbc537c84236
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-03-26 01:10:30 +0530

    Cleanup: Remove class files and apply gitignore

---
+++ 03-controls-loops/01_Area_Of_Circle.java
@@ -0,0 +1,19 @@
+/* 1. Area Of Circle Java Program */
+
+import java.util.Scanner;
+
+class Area_Of_Circle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.print("Enter the value of radius: ");
+        double radius = scanner.nextDouble();
+
+        // Formula: Area = πr²
+        double AreaCircle = Math.PI * radius * radius; 
+
+        System.out.printf("Area of circle is %.2f and radius is %.2f", AreaCircle, radius);
+
+        scanner.close();
+    }
+}
---
+++ 03-controls-loops/02_Area_Of_Triangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Area_Of_Triangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter base: ");
+        double base = scanner.nextDouble();
+
+        System.out.print("Enter height: ");
+        double height = scanner.nextDouble();
+
+        // Formula: ½ × base × height
+        double AreaTriangle = 0.5 * base * height;
+
+        System.out.printf("Area of Triangle is %.2f, base is %.2f and height is %.2f", AreaTriangle, base, height);
+
+        scanner.close();
+    }
+}
