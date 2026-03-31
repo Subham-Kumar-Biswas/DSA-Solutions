ec98cde4ba00e8264e56f044ef1f21c5179e8d06
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-03-31 20:54:06 +0530

    Day 5 solutions added

---
+++ 05-arrays/04_Running_Sum_of_1d_Array.java
@@ -0,0 +1,23 @@
+// Leetcode - #1480 Running of 1d Array
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = runningSum(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+    
+
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
