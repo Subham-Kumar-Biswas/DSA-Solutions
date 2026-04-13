6e9a2ff46e994540bcfce50b35c784bece72addb
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-04-13 19:56:44 +0530

    Day 7 solutions added

---
+++ 07-sorting/01_Contains_Duplicate.java
@@ -0,0 +1,35 @@
+// 217. Contains Duplicate
+// Input: nums = [1,2,3,1]
+// Output: true
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums));
+        
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        int[] sortedArr = bubble(nums);
+
+        for (int i = 0; i < sortedArr.length - 1; i++) {
+            if (sortedArr[i] == sortedArr[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int[] bubble(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length - i; j++) {
+                if (nums[j] < nums[j-1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j - 1];
+                    nums[j - 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
