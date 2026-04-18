dd9db1b558a0df3f918fe5bd5814ca385264f2a5
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-04-18 21:08:28 +0530

    Day 8 solutions added

---
+++ 08-strings/04_Goal_Parser_Interpretation.java
@@ -0,0 +1,28 @@
+// Leetcode - #1678 Goal Parser Interpretation
+// Input: command = "G()(al)"
+// Output: "Goal"
+
+class Solution {
+    public static void main(String[] args) {
+        String command = "G()(al)";
+        System.out.println(interpret(command));
+    }
+
+    public static String interpret(String command) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'G') {
+                builder.append("G");
+            } else if (command.charAt(i) == '(') {
+                if (command.charAt(i+1) == ')') {
+                    builder.append("o");
+                    i++;
+                } else {
+                    builder.append("al");
+                    i+=3;
+                }   
+            } 
+        }
+        return builder.toString();
+    }
+}
