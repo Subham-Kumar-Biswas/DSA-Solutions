983bb2cf4a6d9bced907550df58bf5f89ba965b3
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-04-20 19:58:19 +0530

    Day 10 solutions added

---
+++ 10-recursion/01_Fibonacci_Number.java
@@ -0,0 +1,18 @@
+// Leetcode - #509 Fibonacci Number
+// Input: n = 2
+// Output: 1
+
+class Solution {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(fib(n));
+    }
+    public static int fib(int n) {
+        if (n == 0 ) {
+            return 0;
+        } else if (n <= 2) {
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
