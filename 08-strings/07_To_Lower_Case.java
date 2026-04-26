3a971e0d5fa12d434b86eec18f69c243076ecd9b
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-04-26 21:29:36 +0530

    Day 8 solutions added

---
+++ 08-strings/07_To_Lower_Case.java
@@ -0,0 +1,14 @@
+// Leetcode - #709 To Lower Case
+// Input: s = "Hello"
+// Output: "hello"
+
+class Solution {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(toLowerCase(s));
+    }
+
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
