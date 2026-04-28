8ff161dd9192fe0fe245055ca457032741b14bdb
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-04-28 21:53:59 +0530

    Day 11 solutions added

---
+++ 11-bitwise/02_Number_of_1_Bits.java
@@ -0,0 +1,21 @@
+// Leetcode - #191 Number of 1 Bits
+// Input: n = 11
+// Output: 3
+
+class NumberOfOneBits {
+    public static void main(String[] args) {
+        int n = 11;
+        System.out.println(hammingWeight(n));
+    }
+
+    public static int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>>= 1;
+        }
+        return count;
+    }
+}
