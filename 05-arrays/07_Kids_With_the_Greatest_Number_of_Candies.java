26b52c8797340a7cc484b176694247199520e14c
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-05-08 22:07:43 +0530

    Day 5 solutions added

--- 05-arrays/07_Kids_With_the_Greatest_Number_of_Candies.java
+++ 05-arrays/07_Kids_With_the_Greatest_Number_of_Candies.java
@@ -5,7 +5,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
+class KidsWithCandies {
     public static void main(String[] args) {
         int[] candies = {2,3,5,1,3};
         int extraCandies = 3;
