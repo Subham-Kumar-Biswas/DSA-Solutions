73f8ddb2f205dd8ddd7926d41af6329a19180945
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-05-11 21:35:22 +0530

    Day 7 solutions added

---
+++ 07-sorting/05_Sort_Array_By_Parity.java
@@ -0,0 +1,25 @@
+// Leetcode - #905 Sort Array By Parity
+// Input: nums = [3,1,2,4]
+// Output: [2,4,3,1]
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums = {3,1,2,4};
+        System.out.println(Arrays.toString(sortArrayByParity(nums)));
+    }
+
+    public static int[] sortArrayByParity(int[] nums) {
+        int Switch = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] % 2 == 0) {
+                int temp = nums[i];
+                nums[i] = nums[Switch];
+                nums[Switch] = temp;
+                Switch++;
+            }
+        }
+        return nums;
+    }
+}
