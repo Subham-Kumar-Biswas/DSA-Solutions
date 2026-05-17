bacdc8a2006280244025a3de9ca62fce952ec883
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-05-17 22:31:42 +0530

    Day 5 solutions added

---
+++ 05-arrays/16_Remove_Element.java
@@ -0,0 +1,45 @@
+// Leetcode - #27 Remove Element
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+
+import java.util.Arrays;
+
+class Main {
+    static class Solution {
+        public int removeElement(int[] nums, int val) {
+            int index = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != val) {
+                    nums[index] = nums[i];
+                    index++;
+                }
+            }
+            return index;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solver = new Solution();
+
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        System.out.println("Original array: " + Arrays.toString(nums));
+        System.out.println("Value to remove: " + val);
+
+        int newLength = solver.removeElement(nums, val);
+
+        System.out.println("\n--- Results ---");
+        System.out.println("Returned length (k): " + newLength);
+        
+        System.out.print("Modified array (first " + newLength + " elements): [");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(nums[i]);
+            if (i < newLength - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
